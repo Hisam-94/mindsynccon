1240f3ce2446aab4f55da498f7721559cf93aba6
Hisamuddin Shaikh <hisamuddin94@gmail.com>
2025-01-01 14:57:17 +0530

    made changes to backend

--- backend/server.js
+++ backend/server.js
@@ -17,5 +17,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/items', itemRoutes);
 app.use('/api/bookings', bookingRoutes);
 
+app.get('/', (req, res) => res.send('Hello World!'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
